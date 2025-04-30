971f224cc374125df63dceb7d89cdaf8d8f8011e
mohamadGazer <mohamadgazer@yahoo.com>
2025-04-30 12:20:42 +0300

    gen hive counter model

--- lib/Core/models/counter_down_event/counter_down_event.dart
+++ lib/Core/models/counter_down_event/counter_down_event.dart
@@ -1,18 +1,36 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
+part 'counter_down_event.g.dart';
+
+@HiveType(typeId: 0)
 class CountdownEvent {
+  @HiveField(0)
   final int id;
-  final String title;
-  final DateTime eventDateTime;
-  final String? emoji;
-  final String? backgroundImagePath;
-  final Color? color;
 
-  CountdownEvent(
-      {required this.id,
-      required this.title,
-      required this.eventDateTime,
-      this.emoji,
-      this.backgroundImagePath,
-      this.color});
+  @HiveField(1)
+  final String title;
+
+  @HiveField(2)
+  final DateTime eventDateTime;
+
+  @HiveField(3)
+  final String? emoji;
+
+  @HiveField(4)
+  final String? backgroundImagePath;
+
+  @HiveField(5)
+  final int? colorValue; // هنخزن اللون كـ int
+
+  CountdownEvent({
+    required this.id,
+    required this.title,
+    required this.eventDateTime,
+    this.emoji,
+    this.backgroundImagePath,
+    Color? color,
+  }) : colorValue = color?.value;
+
+  Color? get color => colorValue != null ? Color(colorValue!) : null;
 }
