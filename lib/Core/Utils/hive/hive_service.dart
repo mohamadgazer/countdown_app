8600b7fbd7381be3b1b2728724069f3eac3868a1
mohamadGazer <mohamadgazer@yahoo.com>
2025-05-03 19:29:55 +0300

    set constant file to save string var name

--- lib/Core/Utils/hive/hive_service.dart
+++ lib/Core/Utils/hive/hive_service.dart
@@ -1,4 +1,6 @@
 import 'package:countdown_app/Core/models/counter_down_event/counter_down_event.dart';
+import 'package:countdown_app/constant.dart';
+import 'package:countdown_app/exports.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -12,5 +14,36 @@ class HiveService {
   }
 
   static Box<CountdownEvent> get eventsBox =>
-      Hive.box<CountdownEvent>('events');
+      Hive.box<CountdownEvent>(Constk.events);
+}
+
+void insertDummyEvents() {
+  final box = Hive.openBox(Constk.events);
+  final events = [
+    CountdownEvent(
+      id: 1,
+      title: 'فرحي 🎉',
+      eventDateTime: DateTime.now().add(const Duration(days: 30)),
+      emoji: '💍',
+      color: Colors.pinkAccent,
+    ),
+    CountdownEvent(
+      id: 2,
+      title: 'سفر دبي ✈️',
+      eventDateTime: DateTime.now().add(const Duration(days: 10)),
+      emoji: '🌆',
+      color: Colors.blueAccent,
+    ),
+    CountdownEvent(
+      id: 3,
+      title: 'اجازة العيد 🕌',
+      eventDateTime: DateTime.now().add(const Duration(days: 5)),
+      emoji: '🎊',
+      color: Colors.green,
+    ),
+  ];
+
+  for (final e in events) {
+    database.insertEvent(e);
+  }
 }
